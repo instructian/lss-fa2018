c77eb89d1f7babd6da25b1b6bb40db697937eda0
ian <ianpollock@Arts-VBT-Instructor.local>
2018-11-07 15:43:41 -0800

    multiple background position

---
+++ main.js
@@ -0,0 +1,30 @@
+//console.log("yeah");
+//window.scrollBy(100, 100);
+//alert(window.pageXOffset + window.pageYOffset);
+
+var counter = document.getElementById('counter');
+
+window.onscroll = function(){
+
+                  var distance = window.pageYOffset;
+                  console.log(distance);
+                  document.getElementById('counter').innerHTML = Math.floor(distance / 450)+"<span>k</span>";
+
+
+
+
+                  counter.style.top = 100 + distance +"px";
+                  counter.style.transform="rotate("+ distance +"deg)";
+
+
+
+                  if(distance > 2000){
+                    counter.style.backgroundColor ="red";
+                    } else {
+                    document.getElementById('counter').style.backgroundColor ="navy";
+                    }
+
+
+
+
+      }
